5123873643836774b6ff89a18e86e1d74e74b4ce
Ahmed Ameen <99663431+AhmedAmeen1@users.noreply.github.com>
2023-04-13 15:28:12 +0200

    Update doctors.js

--- routes/doctors.js
+++ routes/doctors.js
@@ -62,7 +62,9 @@ router.delete('/:id', async (req, res) => {
   });
 
 
-
+  router.get('/login', (req,res)=>{
+      res.send('Please type your Email and password')
+      );
   router.post('/login', async (req, res) => {
     const { email, password } = req.body;
   
