cb00258788fadcdeb78b6d72b2edfeb31135fd2a
Ahmed ameen <ahmed.ameen.inf@gmail.com>
2023-05-14 02:13:30 +0200

    fixes

--- routes/diseases.js
+++ routes/diseases.js
@@ -2,4 +2,15 @@ const express = require('express');
 const router = express.Router();
 const Disease = require('../models/Disease');
 
+
+router.get('/' , async (req , res)=>{
+    try {
+        const symptoms = await Disease.findAll();
+        res.status(200).json(symptoms);
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server Error' });
+      }
+    });
+    
 module.exports = router;
