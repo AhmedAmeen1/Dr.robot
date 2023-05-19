fac6d2168b08aa714bf910d530576e33e370ef18
Ahmed ameen <ahmed.ameen.inf@gmail.com>
2023-05-19 19:38:14 +0200

    fixes

--- routes/symptoms.js
+++ routes/symptoms.js
@@ -1,16 +1,47 @@
 const express = require('express');
 const router = express.Router();
 const Symptom = require('../models/Symptom');
+const { Op } = require('sequelize');
 
 
-router.get('/' , async (req , res)=>{
-    try {
-        const symptoms = await Symptom.findAll();
-        res.status(200).json(symptoms);
-      } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Server Error' });
-      }
-    });
-    
+
+const searchSymptoms = async (searchTerm) => {
+
+  const symtpoms = await Symptom.findAll({
+    where: {
+      name: {
+        [Op.like]: `%${searchTerm}%`,
+      },
+    }
+  });
+
+  return symtpoms;
+};
+
+
+router.get('/autocomplete', async (req, res) => {
+  const { searchTerm } = req.query;
+
+  if (!searchTerm) {
+    return res.status(400).json({ message: 'Missing searchTerm parameter' });
+  }
+
+  const symtpoms = await searchSymptoms(searchTerm);
+
+  return res.json(symtpoms.map((symtpom) => symtpom.name));
+});
+
+
+
+router.get('/', async (req, res) => {
+  try {
+    const symptoms = await Symptom.findAll();
+    res.status(200).json(symptoms);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server Error' });
+  }
+});
+
+
 module.exports = router;
