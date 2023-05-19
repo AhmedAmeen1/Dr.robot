14aaa1aea5b363373e6d76782c571f945d4c6b9f
Ahmed ameen <ahmed.ameen.inf@gmail.com>
2023-05-19 19:13:42 +0200

    fixes

--- routes/symptoms.js
+++ routes/symptoms.js
@@ -1,6 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const Symptom = require('../models/Symptom');
+const { Op } = require('sequelize');
+
+
+
+const searchSymptoms = async (searchTerm) => {
+
+  const symtpoms = await Symptom.findAll({
+    where: {
+      name: {
+        [Op.like]: `%${searchTerm}%`,
+      },
+    }
+  });
+
+  return symtpoms;
+};
+
+
+router.get('/autocomplete', async (req, res) => {
+  const { searchTerm } = req.query;
+
+  if (!searchTerm) {
+    return res.status(400).json({ message: 'Missing searchTerm parameter' });
+  }
+
+  const symtpoms = await searchSymptoms(searchTerm);
+
+  return res.json(symtpoms.map((symtpom) => symtpom.name));
+});
+
 
 
 router.get('/' , async (req , res)=>{
