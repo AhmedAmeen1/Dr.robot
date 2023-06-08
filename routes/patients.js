c4c91148ac57120716d35f108b3765c25452faee
Ahmed ameen <ahmed.ameen.inf@gmail.com>
2023-06-08 18:29:33 +0200

    CtrlZ

--- routes/patients.js
+++ routes/patients.js
@@ -8,12 +8,7 @@ router.get('/' , async (req,res)=>{
     const patients = await Patient.findAll({
         include: [{model:Doctor, attributes:['name','specialization', 'phoneNo']}]
     });
-
-    try {
-      res.json(patients);
-    } catch (error) {
-      res.status(400).json(error)
-    }
+    res.json(patients);
 });
 
 router.post('/signup' , async(req,res)=>{try {
@@ -65,78 +60,22 @@ router.delete('/:id', async (req, res) => {
   });
 
   router.put('/:id', async (req, res) => {
-    const { id } = req.params;
-    const {
-      fname,
-      lname,
-      phoneNo,
-      age,
-      gender,
-      email,
-      password,
-      nationalnumber,
-      address,
-      medicalHistory,
-      bloodType,
-      BloodPressure,
-      Diabetes,
-      Allergic,
-      Surgery,
-      Cancer,
-      Pregnant,
-      Smoker,
-      temperature,
-      X_Ray_done,
-      Analysis_done,
-      Medicine_done,
-      Serum_done,
-      Reviewed_devices,
-    } = req.body;
-  
     try {
-      // Find the patient record by ID
-      const patient = await Patient.findByPk(id);
-  
-      if (!patient) {
-        return res.status(404).json({ error: 'Patient not found' });
+      const { id } = req.params;
+      const { name, email, password, phoneNo, gender,age, nationalnumber, address } = req.body;
+      const [updatedRows] = await Patient.update({ name, email, password, phoneNo, gender,age, nationalnumber, address }, { where: { id } });
+      if (updatedRows) {
+        const updatedUser = await Patient.findByPk(id);
+        res.json(updatedUser);
+      } else {
+        res.status(404).json({ message: 'patient not found' });
       }
-  
-      // Update the patient record with new values
-      patient.fname = fname;
-      patient.lname = lname;
-      patient.phoneNo = phoneNo;
-      patient.age = age;
-      patient.gender = gender;
-      patient.email = email;
-      patient.password = password;
-      patient.nationalnumber = nationalnumber;
-      patient.address = address;
-      patient.medicalHistory = medicalHistory;
-      patient.bloodType = bloodType;
-      patient.BloodPressure = BloodPressure;
-      patient.Diabetes = Diabetes;
-      patient.Allergic = Allergic;
-      patient.Surgery = Surgery;
-      patient.Cancer = Cancer;
-      patient.Pregnant = Pregnant;
-      patient.Smoker = Smoker;
-      patient.temperature = temperature;
-      patient.X_Ray_done = X_Ray_done;
-      patient.Analysis_done = Analysis_done;
-      patient.Medicine_done = Medicine_done;
-      patient.Serum_done = Serum_done;
-      patient.Reviewed_devices = Reviewed_devices;
-  
-      // Save the updated patient record
-      await patient.update();
-  
-      return res.json(patient);
     } catch (error) {
       console.error(error);
-      return res.status(500).json({ error: 'Internal server error' });
+      res.status(500).json({ message: 'Server error' });
     }
   });
-  
+
 
   router.get('/:id/appointments', async (req, res) => {
    await Appointment.findAll({
@@ -163,8 +102,7 @@ router.get('/:id/prescriptions', async (req,res)=>{
   try {
     const patient = await Patient.findByPk(patientId);
     const PatientPrescription = await Prescription.findAll({where:{patientId:patient.id},
-       include:[{ model: Patient,attributes:{exclude:['password', 'address']}}]
-      });
+       include:[{ model: Patient,attributes:{exclude:['password', 'address']}}]});
     res.status(200).json(PatientPrescription);
   } catch (error) {
     res.status(400).json(error)
